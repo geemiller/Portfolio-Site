127ac5c9bcb0ee5ab937a85a6ad5b89914664365
gsm524 <gsm524@york.ac.uk>
2023-06-06 22:57:35 +0100

    adding stars in background

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function getFileName(str) {
     return str.substring(str.lastIndexOf('/') + 1);
 }
 
+function createStar() {
+    const star = document.createElement("div");
+    star.style.left = `${Math.random() * 100}%`;
+    star.style.top = `${Math.random() * 100}%`;
+    star.style.animationDuration = `${Math.random() * 5 + 2}s`; 
+    return star;
+  }
+  
+  const numStars = 70; 
+  const starsContainers = document.querySelectorAll("#stars, #stars2, #stars3");
+  
+  starsContainers.forEach((container) => {
+    for (let i = 0; i < numStars; i++) {
+      const star = createStar();
+      container.appendChild(star);
+    }
+  });
+  
